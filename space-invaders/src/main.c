35511cf66c07feb0c516ae99a907076f690c9ef9
Gabriel Enriquez <thegabmeister@protonmail.com>
2026-02-26 00:45:03 +0800

    Include nuklear.h

--- space-invaders/src/main.c
+++ space-invaders/src/main.c
@@ -3,6 +3,17 @@
 #include <SDL3/SDL.h>
 #include <flecs.h>
 
+/* mandatory: sdl3_renderer depends on those defines */
+#define NK_INCLUDE_COMMAND_USERDATA
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+
+#define NK_IMPLEMENTATION
+#include "nuklear.h"
+#define NK_SDL3_RENDERER_IMPLEMENTATION
+#include "nuklear_sdl3_renderer.h"
+
+
+
 #include "texture.h"
 
 typedef struct { float x, y; } Position;
