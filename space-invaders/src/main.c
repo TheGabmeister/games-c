712bb891f3c90e434d5798d976e7bc2e021f714b
Gabriel Enriquez <thegabmeister@protonmail.com>
2026-02-25 23:28:15 +0800

    Refactor texture loading

--- space-invaders/src/main.c
+++ space-invaders/src/main.c
@@ -3,8 +3,7 @@
 #include <SDL3/SDL.h>
 #include <flecs.h>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "texture.h"
 
 typedef struct { float x, y; } Position;
 typedef struct { float w, h; } Size;
@@ -16,20 +15,6 @@ ECS_COMPONENT_DECLARE(Velocity);
 ECS_TAG_DECLARE(Projectile);
 ECS_TAG_DECLARE(Enemy);
 
-static SDL_Texture *load_texture(SDL_Renderer *renderer, const char *path) {
-    int w, h;
-    unsigned char *pixels = stbi_load(path, &w, &h, NULL, 4);
-    if (!pixels) {
-        fprintf(stderr, "Failed to load texture '%s': %s\n", path, stbi_failure_reason());
-        return NULL;
-    }
-    SDL_Texture *tex = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA32,
-                                         SDL_TEXTUREACCESS_STATIC, w, h);
-    SDL_UpdateTexture(tex, NULL, pixels, w * 4);
-    SDL_SetTextureBlendMode(tex, SDL_BLENDMODE_BLEND);
-    stbi_image_free(pixels);
-    return tex;
-}
 
 #define PLAYER_SPEED    300.0f
 #define BULLET_SPEED    600.0f
---
+++ space-invaders/src/texture.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <SDL3/SDL.h>
+
+SDL_Texture *load_texture(SDL_Renderer *renderer, const char *path);
