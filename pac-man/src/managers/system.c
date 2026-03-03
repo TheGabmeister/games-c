9df4a0827f0fdb2daafb029f2c62f7de4b2fb988
Gabriel Enriquez <thegabmeister@protonmail.com>
2026-03-04 03:09:08 +0800

    Fix outdated colons

--- pac-man/src/managers/system.c
+++ pac-man/src/managers/system.c
@@ -39,18 +39,18 @@ void system_manager_init(ecs_world_t *world)
   ECS_SYSTEM(world, process_input, EcsOnLoad, [out] $Input, [inout] $Settings, [in] $Display);
   ECS_SYSTEM(world, gui_input, EcsPostLoad, [inout] $Interface, [in] $Input, [in] $Settings, [in] Window);
   ECS_SYSTEM(world, gui_update, EcsPreUpdate, [inout] $Interface, [in] Window(up), [inout] ?Widget);
-  ECS_SYSTEM(world, transition, EcsPreUpdate, [in] $Time, [inout] Transition, [out] :*);
-  ECS_SYSTEM(world, state_machine, EcsPreUpdate, [in] $Time, [inout] Stateful, [out] :*);
+  ECS_SYSTEM(world, transition, EcsPreUpdate, [in] $Time, [inout] Transition, [out] *());
+  ECS_SYSTEM(world, state_machine, EcsPreUpdate, [in] $Time, [inout] Stateful, [out] *());
   ECS_SYSTEM(world, update_viewport, EcsOnUpdate, [in] $Input, [inout] Viewport);
-  ECS_SYSTEM(world, update_scene, EcsOnUpdate, [inout] Scene, [inout] Stateful, [out] Transition, [inout] $Time, [in] $Input, [inout] $Settings, [out] :*);
-  ECS_SYSTEM(world, physics_update, EcsOnValidate, [in] $Time, [inout] $Physics, [out] :Collision);
-  ECS_SYSTEM(world, physics_collide, EcsOnValidate, [in] $Physics, [inout] Collision, [out] :*);
+  ECS_SYSTEM(world, update_scene, EcsOnUpdate, [inout] Scene, [inout] Stateful, [out] Transition, [inout] $Time, [in] $Input, [inout] $Settings, [out] *());
+  ECS_SYSTEM(world, physics_update, EcsOnValidate, [in] $Time, [inout] $Physics, [out] Collision());
+  ECS_SYSTEM(world, physics_collide, EcsOnValidate, [in] $Physics, [inout] Collision, [out] *());
 #ifdef DEBUG
-  ECS_SYSTEM(world, debug_input, EcsPostUpdate, [in] $Input, [out] $Debug, [out] :*);
-  ECS_SYSTEM(world, debug_scene, EcsPostUpdate, [in] Scene, [in] Stateful, [out] $Debug, [out] :*);
+  ECS_SYSTEM(world, debug_input, EcsPostUpdate, [in] $Input, [out] $Debug, [out] *());
+  ECS_SYSTEM(world, debug_scene, EcsPostUpdate, [in] Scene, [in] Stateful, [out] $Debug, [out] *());
 #endif
   ECS_SYSTEM(world, play_music, EcsPreStore, [inout] Track);
-  ECS_SYSTEM(world, play_sounds, EcsPreStore, [inout] Audible, [out] :*);
+  ECS_SYSTEM(world, play_sounds, EcsPreStore, [inout] Audible, [out] *());
   ECS_SYSTEM(world, animate, EcsOnStore, [inout] Animated, [out] Renderable);
   ECS_SYSTEM(world, render_scene, EcsOnStore, [in] Scene, [in] Stateful, [in] Transition, [out] $Display, [in] $Time);
   ECS_SYSTEM(world, render_viewports, EcsOnStore, [in] Viewport);
@@ -60,7 +60,7 @@ void system_manager_init(ecs_world_t *world)
   ECS_SYSTEM(world, render_labels, EcsOnStore, [in] $Time, [in] Label, [in] Aligned, [in] Spatial, [in] Tinted);
 #ifdef DEBUG
   ECS_SYSTEM(world, debug_physics, EcsOnStore, [in] $Physics, [in] $Debug, [inout] Viewport);
-  ECS_SYSTEM(world, debug_render, EcsOnStore, [in] Label, [in] $Debug, DebugTag, [out] :*);
+  ECS_SYSTEM(world, debug_render, EcsOnStore, [in] Label, [in] $Debug, DebugTag, [out] *());
   ECS_SYSTEM(world, debug_display, EcsOnStore, [in] $Display, [in] $Debug);
 #endif
   ECS_SYSTEM(world, composite_display, EcsOnStore, [in] $Display);
