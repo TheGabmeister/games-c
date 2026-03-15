6b5b6dc2e4cf53caf366e8e2a2e9611358577ee1
TheGabmeister <thegabmeister@protonmail.com>
2026-03-15 18:48:07 +0800

    Update shape.h

--- pong/src/components/shape.h
+++ pong/src/components/shape.h
@@ -5,11 +5,23 @@
 #include <SDL3/SDL.h>
 #include "../platform.h"
 
+typedef enum ShapeType
+{
+    SHAPE_RECTANGLE,
+    SHAPE_CIRCLE,
+    SHAPE_TRIANGLE
+} ShapeType;
+
 typedef struct Shape
 {
-    float width;
-    float height;
+    ShapeType type;
     SDL_Color color;
+    union
+    {
+        struct { float width; float height; } rectangle;
+        struct { float radius; } circle;
+        struct { float base; float height; } triangle;
+    };
 } Shape;
 
 ECS_COMPONENT_DECLARE(Shape);
