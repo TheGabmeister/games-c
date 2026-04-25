c4e1ac9271f73b1fa92cd4fc602628dffb3959c8
Gabriel Enriquez <thegabmeister@protonmail.com>
2026-04-25 20:38:09 +0800

    Update sounds.c

--- raylib/src/sounds.c
+++ raylib/src/sounds.c
@@ -5,29 +5,13 @@ static const char *sound_files[SOUND_COUNT] = {
 };
 
 void sounds_load(Game *game) {
-    game->sounds_loaded = false;
-    for (int i = 0; i < SOUND_COUNT; i++) {
-        if (FileExists(sound_files[i])) {
-            game->sounds[i] = LoadSound(sound_files[i]);
-            game->sounds_loaded = true;
-        }
-    }
+
 }
 
 void sounds_unload(Game *game) {
-    for (int i = 0; i < SOUND_COUNT; i++) {
-        if (IsSoundValid(game->sounds[i])) {
-            UnloadSound(game->sounds[i]);
-        }
-    }
+
 }
 
 void sound_play(Game *game, SoundID id) {
-    if (id < 0 || id >= SOUND_COUNT) {
-        return;
-    }
 
-    if (IsSoundValid(game->sounds[id])) {
-        PlaySound(game->sounds[id]);
-    }
 }
